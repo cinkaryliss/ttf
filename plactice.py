a018190950d88755ac0318974c3eb5c63440c245
Yoshi <cinkaryliss@gmail.com>
2017-05-03 16:26:46 +0900

    change

---
+++ plactice.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from tensorflow.contrib.keras.python.keras.datasets import mnist
+from tensorflow.contrib.keras.python.keras.models import Sequential
+from tensorflow.contrib.keras.python.keras.layers import Dense, Dropout, Activation
+from tensorflow.contrib.keras.python.keras.optimizers import Adam
+from tensorflow.contrib.keras.python.keras.utils import np_utils
+from tensorflow.contrib.keras.python.keras import backend as K
